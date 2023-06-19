fe8dfdcbd2eb47ef5b06ac49492d5352a0773d45
ahmadriski23 <ahmadriskixrpl1csr@gmail.com>
2023-06-19 14:51:40 +0700

    update color and ui

--- lib/const.dart
+++ lib/const.dart
@@ -1,4 +1,4 @@
 import 'package:flutter/material.dart';
 
-final kPrimaryColor = Color.fromARGB(255, 74, 12, 220);
-final kShadowColor = Color.fromARGB(255, 103, 38, 255);
+final kPrimaryColor = Color.fromARGB(255, 27, 89, 188);
+final kShadowColor = Color.fromARGB(255, 67, 119, 250);
--- lib/widgets/headHomePage.dart
+++ lib/widgets/headHomePage.dart
@@ -93,159 +93,154 @@ class _HeadHomePageState extends State<HeadHomePage> {
           ],
         ),
         Container(
-          child: Padding(
-            padding: const EdgeInsets.only(top: 140),
-            child: Container(
-              child: Positioned(
-                child: Align(
-                  alignment: Alignment.topCenter,
-                  child: Padding(
-                    padding: const EdgeInsets.fromLTRB(50, 0, 50, 0),
-                    child: Container(
-                      height: 170,
-                      width: MediaQuery.of(context).size.width,
-                      decoration: BoxDecoration(
-                        boxShadow: [
-                          BoxShadow(
-                            color: Color.fromRGBO(47, 125, 121, 0.3),
-                            offset: Offset(0, 6),
-                            blurRadius: 12,
-                            spreadRadius: 6,
-                          )
-                        ],
-                        color: kShadowColor,
-                        borderRadius: BorderRadius.circular(15),
+          child: Positioned(
+            child: Align(
+              alignment: Alignment.topCenter,
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(50, 140, 50, 0),
+                child: Container(
+                  height: 170,
+                  width: MediaQuery.of(context).size.width,
+                  decoration: BoxDecoration(
+                    boxShadow: [
+                      BoxShadow(
+                        color: Color.fromRGBO(47, 125, 121, 0.3),
+                        offset: Offset(0, 6),
+                        blurRadius: 12,
+                        spreadRadius: 6,
+                      )
+                    ],
+                    color: kShadowColor,
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        height: 10,
                       ),
-                      child: Column(
-                        children: [
-                          SizedBox(
-                            height: 10,
-                          ),
-                          Padding(
-                            padding: EdgeInsets.symmetric(horizontal: 15),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 15),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              'Total Balance',
+                              style: TextStyle(
+                                  fontSize: 13,
+                                  fontWeight: FontWeight.w500,
+                                  color: Colors.white),
+                            ),
+                            Icon(
+                              Icons.more_horiz,
+                              color: Colors.white,
+                            )
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        height: 7,
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(left: 15),
+                        child: Row(
+                          children: [
+                            Text(
+                              '\$ ${total()}',
+                              style: TextStyle(
+                                  fontSize: 17,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white),
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        height: 25,
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(left: 15, right: 15),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Row(
                               children: [
+                                CircleAvatar(
+                                  radius: 13,
+                                  backgroundColor: Colors.white,
+                                  child: Icon(
+                                    Icons.arrow_downward,
+                                    color: kPrimaryColor,
+                                    size: 16,
+                                  ),
+                                ),
+                                SizedBox(
+                                  width: 7,
+                                ),
                                 Text(
-                                  'Total Balance',
+                                  'Income',
                                   style: TextStyle(
                                       fontSize: 13,
                                       fontWeight: FontWeight.w500,
                                       color: Colors.white),
                                 ),
-                                Icon(
-                                  Icons.more_horiz,
-                                  color: Colors.white,
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                CircleAvatar(
+                                  radius: 13,
+                                  backgroundColor: Colors.white,
+                                  child: Icon(
+                                    Icons.arrow_upward,
+                                    color: kPrimaryColor,
+                                    size: 16,
+                                  ),
+                                ),
+                                SizedBox(
+                                  width: 7,
+                                ),
+                                Text(
+                                  'Expenses',
+                                  style: TextStyle(
+                                      fontSize: 13,
+                                      fontWeight: FontWeight.w500,
+                                      color: Colors.white),
                                 )
                               ],
                             ),
-                          ),
-                          SizedBox(
-                            height: 7,
-                          ),
-                          Padding(
-                            padding: EdgeInsets.only(left: 15),
-                            child: Row(
-                              children: [
-                                Text(
-                                  '\$ ${total()}',
-                                  style: TextStyle(
-                                      fontSize: 17,
-                                      fontWeight: FontWeight.bold,
-                                      color: Colors.white),
-                                ),
-                              ],
-                            ),
-                          ),
-                          SizedBox(
-                            height: 25,
-                          ),
-                          Padding(
-                            padding: EdgeInsets.only(left: 15, right: 15),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Row(
-                                  children: [
-                                    CircleAvatar(
-                                      radius: 13,
-                                      backgroundColor: Colors.white,
-                                      child: Icon(
-                                        Icons.arrow_downward,
-                                        color: kPrimaryColor,
-                                        size: 16,
-                                      ),
-                                    ),
-                                    SizedBox(
-                                      width: 7,
-                                    ),
-                                    Text(
-                                      'Income',
-                                      style: TextStyle(
-                                          fontSize: 13,
-                                          fontWeight: FontWeight.w500,
-                                          color: Colors.white),
-                                    ),
-                                  ],
-                                ),
-                                Row(
-                                  children: [
-                                    CircleAvatar(
-                                      radius: 13,
-                                      backgroundColor: Colors.white,
-                                      child: Icon(
-                                        Icons.arrow_upward,
-                                        color: kPrimaryColor,
-                                        size: 16,
-                                      ),
-                                    ),
-                                    SizedBox(
-                                      width: 7,
-                                    ),
-                                    Text(
-                                      'Expenses',
-                                      style: TextStyle(
-                                          fontSize: 13,
-                                          fontWeight: FontWeight.w500,
-                                          color: Colors.white),
-                                    )
-                                  ],
-                                ),
-                              ],
-                            ),
-                          ),
-                          Padding(
-                            padding: EdgeInsets.symmetric(
-                                horizontal: 40, vertical: 6),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Text(
-                                  "\$ ${income()}",
-                                  style: TextStyle(
-                                      fontSize: 14,
-                                      fontWeight: FontWeight.w600,
-                                      color: Colors.white),
-                                ),
-                                Text(
-                                  "\$ ${expenses()}",
-                                  style: TextStyle(
-                                      fontSize: 14,
-                                      fontWeight: FontWeight.w600,
-                                      color: Colors.white),
-                                ),
-                              ],
-                            ),
-                          )
-                        ],
+                          ],
+                        ),
                       ),
-                    ),
+                      Padding(
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 40, vertical: 6),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              "\$ ${income()}",
+                              style: TextStyle(
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.w600,
+                                  color: Colors.white),
+                            ),
+                            Text(
+                              "\$ ${expenses()}",
+                              style: TextStyle(
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.w600,
+                                  color: Colors.white),
+                            ),
+                          ],
+                        ),
+                      )
+                    ],
                   ),
                 ),
               ),
             ),
           ),
-        )
+        ),
       ],
     );
   }
